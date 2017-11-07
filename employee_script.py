9b57d5b949431de5be4df1152ac69c0fad336e06
vigneshk <xxvignesh@gmail.com>
2017-11-08 00:20:04 +0530

    changes

---
+++ employee_script.py
@@ -0,0 +1,56 @@
+from pas.models import Employee
+
+employee = '''
+Ela Christon
+Sebastian Pennington
+Migdalia Durrance
+Stephane Fahnestock
+Carl Goulding
+Marielle Eisenhower
+Corine Fennel
+Tammi Darity
+Antonio Galban
+Yolando Hilliker
+Emmett Slape
+Lorriane Pimentel
+Teddy Melrose
+Tommy Payan
+Gisele Nardi
+Tijuana Harder
+Carri Simard
+Luba Cottrell
+Dede Dilley
+Rich Valla
+Nathanial Nocera
+Salley Sampson
+Aleta Wyche
+Alejandra Bertone
+Holli Shattuck
+Jong Beaton
+Alisia Encinas
+Stewart Fregoso
+Sacha Lytch
+Caroline Cool
+Leland Shaddix
+Marcia Hiney
+Roselia Hix
+Sheryll Dominguez
+Suanne Loftin
+Weldon Boehm
+Leandra Jaso
+Kaye Zachery
+Fatima Tullius
+Sindy Watkins
+Justina Nolette
+Annette Severt
+Cliff Lakin
+Drema Blubaugh
+Lee Reynold
+Marhta Crosland
+Granville Reaves
+Na Shealey
+Veda Beaston
+Lucila Schall
+'''
+
+print(employee.split('\n'))
